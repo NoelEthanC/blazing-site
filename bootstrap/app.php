506fb1968943e0d8f8e6c03d6e878fcbc069b36b
NoelEthanC <noelchiwamba1@gmail.com>
2025-07-06 12:46:47 +0200

    v1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->trustProxies(
             at: '*', // Or the IP of your proxy/load balancer
-            headers: Request::HEADER_X_FORWARDED_ALL,
+            // headers: SymfonyRequest::HEADER_FORWARDED
         );
         //
     })
